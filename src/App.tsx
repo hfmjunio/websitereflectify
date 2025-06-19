48d8b39a08ccad8608c0d19130d5c7ab22c55b18
hfmjunio <haniejunio19@gmail.com>
2025-06-19 18:22:48 +0800

    6/19 ud

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignupPage from "./SignupPage";
 import WcPage from "./WcPage";
 import LoginPage from "./LoginPage";
+import StudDBPage from "./StudDBPage";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Route path="/" element={<WcPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
-
+      <Route path="/studentdashboard" element={<StudDBPage />} />
     </Routes>
     </>
     
--- src/WcPage.tsx
+++ src/WcPage.tsx
@@ -9,9 +9,13 @@ function WcPage() {
     navigate('/signup')
   }
 
+    const handleTest = () => {
+    navigate('/studentdashboard')
+  }
+
   return (
     <>
-      <header>
+      <header className='wpheader'>
         <link href="https://fonts.googleapis.com/css2?family=Alata&family=Black+Han+Sans&family=DM+Sans:ital,opsz,wght@0,9..40,523;1,9..40,523&family=Tilt+Warp:XROT@-3&display=swap"rel="stylesheet"/>
         <img src={ReflectifyIcon} alt="Reflectify Logo" />
         <h1>Reflectify</h1>
@@ -21,6 +25,11 @@ function WcPage() {
       <div className="wpbutton">
         <button className="proceedbtn" onClick={handleProceed}>
           Proceed
+        </button>
+      </div>
+      <div className="wptstbutton">
+      <button className="tstbtn" onClick={handleTest}>
+          Try the Demo Version
         </button>
       </div>
     </>
